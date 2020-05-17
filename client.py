4aeff4f6cab69955c6fe93898ad2ff94632ebe84
Dat Luu <tad.louis2810@gmail.com>
2020-05-17 18:50:25 +0700

    update login, signup, verify

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 
-eradio_client = Flask(__name__)
+eradio_client = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 
 @eradio_client.route('/')
 @eradio_client.route('/Account/Login')
 def login():
-	return render_template('login.html', title='Sign In')
+	return render_template('login.html', title='Sign In', isBackground=True)
 
 @eradio_client.route('/Account/SignUp')
 def sign_up():
-	return 'SignUp'
+	return render_template('signup.html', title='Sign Up', isBackground=True)
+
+@eradio_client.route('/Account/Verify')
+def verify():
+	return render_template('verify.html', isBackground=True)
